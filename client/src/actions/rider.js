03b1be604f63b78afd1b4a6393765d39976519cc
Ryoto <willcarter822@gmail.com>
2021-01-25 21:30:34 +0300

    front
    upp

--- client/src/actions/rider.js
+++
@@ -1,5 +0,0 @@
-export const setRiders = (riders) =>({
-    type: 'SET_RIDER',
-    payload : riders
-
-});
--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import riders from "./riders";
-import cart from './cart'
-import filter from "./filter";
-export default combineReducers({
-    riders,
-    cart,
-    filter,
-    });
